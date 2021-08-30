41b18a094b9eea2c2049f30f2a0a36cd4d41d417
Elazar-Goldman <elazargoldman6@gmail.com>
2021-08-30 09:20:02 +0300

    first Push

---
+++ README.txt
@@ -0,0 +1,56 @@
+This project has two main pages
+Search – Allow the user to fetch pictures.
+Home – Favourite pictures display page
+
+//// IT SHOULD BE NOTED////
+All pictures are from https://pixabay.com/. 
+Notation is as per Pixabay’s request
+
+///// Search page////
+
+User can enter a value in the input. 
+By clicking the Submit button, user will receive pictures as per his request. 
+By clicking the Refresh button, user will be able to add a second value to be fetched.
+By clicking Display Pictures, user will be moved to the Home page to view pictures.
+
+//// Selecting pictures///
+
+While hovering over a picture, picture will display:
+Name of creator of the picture.
+An add button.
+User can click the add button, and the picture will be transferred to Favourite list. 
+An indication will show that the picture was selected
+User will not be able to select the same picture twice.
+A counter will appear on top, indicating the amount of selected pictures
+This counter will maintain the count, even if user changes the value of the fetch. 
+
+//// Home page///
+
+All of the selected pictures will be displayed on the home page.
+User can delete pictures by hovering over the picture, and clicking the delete button. 
+
+//// Refreshing the Home page///
+
+User can refresh the home page, and still see his selected pictures. 
+User can return to search page by clicking the + search button as per request. 
+Nav bar was omitted, and navigation is achieved by the stated button. 
+(I am not sure per request, however if there was a desire for a Nav bar I am happy to add a responsive Nav bar)
+
+///// Removing items from localStorage/////
+A button was added to the bottom of the home page, 
+allowing to easily remove stored items from user's device.
+This button is for convenience purpose AND NOT PART OF THE ASSIGNMENT.
+
+/// Methods used////
+
+This project used Fetch as per request
+This project displayed programing of both OOP and non-OOP, in order to show capabilities. 
+Navigation is achieved via buttons as per request.
+
+////Personal note/////
+
+I enjoyed this assignment very much and thank you for the opportunity. 
+The UI/UX were somewhat kept bear. Most attention was dedicated to functionality.
+I hope you enjoy the review. 
+
+
---
+++ js/site.js
@@ -0,0 +1,105 @@
+let data =[];
+
+//Receiving data from storage
+let jsonData = localStorage.getItem('picArrs')
+if(jsonData){
+    data = JSON.parse(jsonData)
+}
+
+// Adding an obj to the end of arr for "search new pictures"
+data.push({index:17})
+//Adding flags
+let iterFlag=0;
+let flagnum =-1;
+let flagBtn =0;
+// Rendering page
+let elem = document.getElementById('innerDiv');
+
+let render =(_arr)=>{
+    elem.innerHTML = "";
+    let picData =_arr
+  for(let x=0;x<picData.length;x++){
+    let item =picData[x];
+    if(x % 10===0){
+        elem.innerHTML+=`<div class="wrap-boxes" id="row"></div>`;
+        flagnum++;
+
+    }
+    let newElem = document.getElementsByClassName(`wrap-boxes`)
+
+    if(item.index!==17){
+        newElem[flagnum].innerHTML+=`
+        <div class="exDiv">
+        <div class="wrapImg" id="wrap${item.id}"></div>
+        </div>
+       `
+       let addDiv =document.getElementById(`wrap${item.id}`);
+       addDiv.style.background = `url(${item.img}) no-repeat center center`;    
+    }else{
+        // adding in the search button (button will be added only once.)
+        if(iterFlag===0){
+            newElem[flagnum].innerHTML+=`
+            <div id="serchDiv" onClick="directSerch()">+</div>
+            ` 
+            iterFlag=1
+        }
+    }
+   
+  }
+  addListners()
+
+}
+
+// adding the functionality needed on hover. 
+let addListners=()=>{
+    let elems = document.getElementsByClassName('wrapImg');
+    for(let x=0;x<elems.length;x++){
+        elems[x].addEventListener('mouseover',(evt)=>{
+            let item =evt.target;
+            if(flagBtn===1 || x> elems.length-1){return}
+            item.innerHTML+=`
+            <button type="button" class="boxBtn btn btn-danger" id="${data[x].id}" onclick="removePic(this.id)">Remove</button>
+            `
+            flagBtn=1;
+        })
+        elems[x].addEventListener('mouseleave',(evt)=>{
+            let item =evt.target
+            let id = item.children[0].id;
+            console.log(id);
+          document.getElementById(id).remove();
+           flagBtn=0;
+        })
+    }
+}
+
+
+let removePic=(_id)=>{
+let newData = data.filter(item=>{return item.id!=_id})
+data = newData;
+flagBtn=0;
+iter=0;
+flagnum=-1;
+iterFlag=0
+render(data);
+
+localStorage.removeItem('picArrs');
+localStorage.setItem('picArrs', JSON.stringify(data));
+}
+
+const directSerch=()=>{
+    window.location.href ="/search.html"
+}
+
+render(data);
+
+const removeFromLocal=()=>{
+localStorage.removeItem('picArrs')
+let removeElem = document.getElementById('removeItem');
+removeElem.innerHTML+= `<div id="removeMsg" style="background-color:red;">
+Items have been removed from your local storage
+</div>`
+setTimeout(()=>{
+    document.getElementById('removeMsg').remove();
+},2000)
+
+}
